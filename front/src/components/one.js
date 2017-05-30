910ba4ce96f1afb86ff864de1e49731e2ee50b60
Korsunow Konstantin <korsunowk@yandex.ua>
2017-05-30 17:14:29 +0300

    Fix table row with infinitive

--- front/src/components/one.js
+++ front/src/components/one.js
@@ -4,7 +4,7 @@ export default class One extends React.Component {
     render () {
         return (
             <tr>
-                <td>{this.props.infinitiv}</td>
+                <td>{this.props.infinitive}</td>
                 <td>{this.props.past_simple}</td>
                 <td>{this.props.perfect}</td>
                 <td>{this.props.russian}</td>
