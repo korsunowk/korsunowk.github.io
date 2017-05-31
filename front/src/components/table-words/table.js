1709e6f45ab0a845838eb5a898a65a2b758b2541
Korsunow Konstantin <korsunowk@yandex.ua>
2017-05-31 11:58:59 +0300

    Add search by number #2

--- front/src/components/table-words/table.js
+++ front/src/components/table-words/table.js
@@ -6,23 +6,59 @@ import '../../styles/table.css';
 var words = require('../../words.json');
 
 export default class WordsTable extends React.Component {
+    constructor() {
+        super();
+
+        let _words = new Object();
+        for(let i=1; i<= Object.keys(words).length; i++)
+            _words[i] = words[i];
+        this.state = {
+            _words: _words
+        };
+        this.Search = this.Search.bind(this);
+    }
+
+    Search(e) {
+        if (e.target.value !== "") {
+            let new_words = new Object();
+            for(let i=1; i<=Object.keys(words).length; i++) {
+                if (e.target.value == i.toString())
+                    new_words[i] = words[i];
+            }
+            this.setState({
+                _words: new_words
+            })
+        }
+        else {
+            this.setState({
+                _words: words
+            })
+        }
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return !(this.state._words == nextState)
+    }
+
     render () {
         let cells = new Array();
-        for(let i=1; i<=Object.keys(words).length; i++) {
+        // console.log(this.state._words);
+        for(let key in this.state._words) {
             cells.push(
                 <TableCell
-                    russian={words[i]['russian_translate']} 
-                    infinitive={words[i]['infinitive']}
-                    past_simple={words[i]['past_simple']}
-                    perfect={words[i]['perfect']}
-                    key={i}
-                    number={i}
+                    russian={this.state._words[key]['russian_translate']}
+                    infinitive={this.state._words[key]['infinitive']}
+                    past_simple={this.state._words[key]['past_simple']}
+                    perfect={this.state._words[key]['perfect']}
+                    key={key}
+                    number={key}
                 />
             )
         }
+        console.log("Render------");
         return (
             <div>
-                <SearchBlock />
+                <SearchBlock onChange={this.Search} />
                 <table>
                     <tbody>
                         <tr className="table-header">
